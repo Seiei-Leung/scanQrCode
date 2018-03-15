48bd6eb98bb5796ea4e543bd327208c57215343a
Seiei-Leung <786883603@qq.com>
2018-03-15 17:41:10 +0800

    修复安卓问题

--- js/common.js
+++ js/common.js
@@ -1,4 +1,10 @@
+
+// 点击链接
+function clicked(id) {
+	var _openw = plus.webview.create(id, id, {scrollIndicator:'none',scalable:false,popGesture:'close'});
+	_openw.show("pop-in", null);
+}
 // 返回
 function goback() {
-	window.history.go(-1);
+	plus.webview.currentWebview().close("slide-out-left", null);
 }
