bb559f7ed9bbe4df93c378a4c9a04cf562dabb1c
Seiei-Leung <786883603@qq.com>
2018-03-16 17:42:16 +0800

    修bug

--- js/common.js
+++ js/common.js
@@ -1,8 +1,13 @@
 
 // 点击链接
 function clicked(id) {
-	var _openw = plus.webview.create(id, id, {scrollIndicator:'none',scalable:false,popGesture:'close'});
-	_openw.show("pop-in", null);
+	if (plus.webview.getWebviewById(id)) {
+		plus.webview.getWebviewById(id).show("pop-in", null);
+	} else {
+		var _openw = plus.webview.create(id, id, {scrollIndicator:'none',scalable:false,popGesture:'close'});
+		_openw.show("pop-in", null);
+	}
+	
 }
 // 返回
 function goback() {
