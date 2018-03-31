a2f09feb42eba7ceeffaba9389c47a64e294ba74
Seiei-Leung <786883603@qq.com>
2018-03-31 11:05:29 +0800

    添加服务器接口

--- js/common.js
+++ js/common.js
@@ -1,20 +1,42 @@
-
 // 点击链接
 function clicked(id) {
-	if (plus.webview.getWebviewById(id)) {
+	if(plus.webview.getWebviewById(id)) {
 		plus.webview.getWebviewById(id).show("pop-in", null);
 	} else {
-		var _openw = plus.webview.create(id, id, {scrollIndicator:'none',scalable:false,popGesture:'close'});
+		var _openw = plus.webview.create(id, id, {
+			scrollIndicator: 'none',
+			scalable: false,
+			popGesture: 'close'
+		});
 		_openw.show("pop-in", null);
 	}
-	
+
 }
 // 返回
 function goback() {
 	plus.webview.currentWebview().close("slide-out-left", null);
 }
 
-//
+// 使用原生跳转链接
 function clickLink(link) {
 	location.assign(link);
+}
+
+// post方法
+function submitPost(url, data, callback) {
+	xhr = new plus.net.XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState == 4) {
+			if(xhr.status == 200) {
+				callback(xhr.responseText);
+			} else {
+				alert('链接服务器错误，上传失败');
+				console.log("responseText: " + xhr.response);
+			}
+		}
+	}
+	xhr.open("POST", url);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	console.log(JSON.stringify(data));
+	xhr.send(JSON.stringify(data));
 }
